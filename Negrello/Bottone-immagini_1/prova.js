4bc51e97a430a821550b8936656bd622c13e4c53
DavidNegrello <121225931+DavidNegrello@users.noreply.github.com>
2024-04-18 19:04:29 +0200

    Update prova.js

--- Negrello/Bottone-immagini_1/prova.js
+++ Negrello/Bottone-immagini_1/prova.js
@@ -1,4 +1,4 @@
-const greetingElement = document.getElementById('greeting');
+const greetingElement = document.getElementById('saluti');
 const changeButton = document.getElementById('change-button');
 
 changeButton.addEventListener('click', () => {
