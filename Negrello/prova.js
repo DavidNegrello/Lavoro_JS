786c0e6a36eec422ff4f091cc9597b97cac8cbfe
DavidNegrello <david.negrello@iisviolamarchesini.edu.it>
2024-04-18 17:18:09 +0200

    Rimosso

--- Negrello/prova.js
+++
@@ -1,6 +0,0 @@
-const greetingElement = document.getElementById('greeting');
-const changeButton = document.getElementById('change-button');
-
-changeButton.addEventListener('click', () => {
-    greetingElement.textContent = 'Hello, JavaScript!';
-});
