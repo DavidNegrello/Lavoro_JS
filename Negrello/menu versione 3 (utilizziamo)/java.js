c06c7957efcfc496c7340a006c5150ff6ca8a9d7
DavidNegrello <121225931+DavidNegrello@users.noreply.github.com>
2024-04-20 10:16:34 +0200

    Fatto il menu che probabilmente utilizziamo

---
+++ Negrello/menu versione 3 (utilizziamo)/java.js
@@ -0,0 +1,8 @@
+//const burgerMenu = document.querySelector('.burger-menu');
+//const navLinks = document.querySelector('.nav-links');
+
+//console.log('JavaScript code is running');
+
+//burgerMenu.addEventListener('click', () => {
+//navLinks.classList.toggle('open');
+//});
