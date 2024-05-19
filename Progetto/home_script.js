676d61c8220a43339e6ab3430b527b95146a811a
DavidNegrello <david.negrello@iisviolamarchesini.edu.it>
2024-05-19 12:33:02 +0200

    inserito il bottone di login

--- Progetto/home_script.js
+++ Progetto/home_script.js
@@ -1,8 +1,4 @@
-//codice per gestire la barra di ricerca nell'apertura o chiusura quandi si preme
-let ricerca = document.querySelector('.search-box');
-document.querySelector('#search-icona').onclick = () =>{
-    ricerca.classList.toggle('attivo');
-}
+
 
 //======================per cambiare la mappa====================
 
